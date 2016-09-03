054ec07f76ddaff5aa6ded3461ae16513468af76
ampleeve <e@ampleev.com>
2016-09-03 16:18:43 +0300

    fix

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
 <?php
-    //echo "Hello world!";
-    //echo time();
-
-    function add($param1, $param2){
-        
-        return $param1 + $param2;
-    }
-
-    $a = $_GET ['a'];
-    $b = $_GET ['b'];
-
-    for ($i=0; $i<11; $i++){
-        echo $i."<br>";
-    }
-
-    echo "<br>".add($a, $b);
+    require_once("database.php");
+    require_once("models/articles.php");
 ?>
---
+++ models/articles.php
@@ -0,0 +1,25 @@
+<?php
+    //echo "Hello world!";
+    //echo time();
+
+    function articlesAll(){
+        
+    }
+     
+    function articlesGet($id){
+        
+    }
+
+    function articlesNew($title, $date, $content){
+        
+    }
+
+    function articlesEdit($id, $title, $date, $content){
+        
+    }
+
+    function articlesDelete($id){
+        
+    }
+
+?>
